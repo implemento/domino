fd32459bafa2b2ce1092a9ddcca2da6e955df0bf
Kristian Du <kristian.du@gmail.com>
2016-10-06 16:03:16 +0200

    Introducing sublime project files, anaconda miniserver support configurations for remote python interpreter autocompletion, docker-py for spinning up new containers from django apps

--- app/servers/models.py
+++ app/servers/models.py
@@ -2,24 +2,37 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=300)
+    name = models.CharField(max_length=300, default='')
+    extended_name = models.CharField(max_length=300, default='')
+
+    def __str__(self):
+        return self.name
 
 
 class Brand(models.Model):
-    pass
+    name = models.CharField(max_length=300, default='')
+
+    def __str__(self):
+        return self.name
 
 
 class Server(models.Model):
-    customer_id = models.ForeignKey(
-        'Customer',
+    customer = models.ForeignKey(
+        Customer,
         on_delete=models.CASCADE
     )
-    brand_id = models.ForeignKey(
-        'Brand',
+    brand = models.ForeignKey(
+        Brand,
         on_delete=models.CASCADE
     )
-    hostname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='')
     model = models.CharField(max_length=100)
+    model.blank = True
     serial_number = models.CharField(max_length=100)
+    serial_number.blank = True
     ip_address = models.GenericIPAddressField()
     enabled = models.BooleanField(default=True)
+    requires_vpn = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
---
+++ start-anaconda-miniserver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker-compose exec as /opt/anaconda/anaconda_server/docker/start python 19360 docker_project
